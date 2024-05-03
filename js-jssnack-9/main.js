a8356db5af4dc40af67d5d327d03319e67fd7e49
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-05-03 19:10:14 +0200

    [UPD]  exercise done

---
+++ js-jssnack-9/main.js
@@ -0,0 +1,22 @@
+// reset js
+'use strict';
+
+console.log("ciao mondo");
+
+// creo un array contentente almeno 10 numeri 
+
+const numbers = [4120,2547,7452,852,25,7452,8541,10,542,25,987,45,60,54,252];
+
+
+let somma = 0 ;
+
+// ciclo sugli elementi di numbers e li sommo 
+for (let i = 0; i < 10 ; i++) {
+    
+    somma = somma + numbers[i];
+    
+}
+// calcolo la media 
+let medianumbers = somma / 10;
+// stampo la media in console 
+console.log(medianumbers);
---
+++ js-jssnack-9/readme.md
@@ -0,0 +1,3 @@
+Calcola la somma e la media dei primi 10 numeri.
+
+Stampa i risultati on console
